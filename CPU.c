ffb1c067adc9ee2798e4af396b939087392d4a40
jeh170 <jeh170@pitt.edu>
2018-02-02 12:54:45 -0500

    Putting in initial files

---
+++ CPU.c
@@ -0,0 +1,114 @@
+/**************************************************************/
+/* CS/COE 1541				 			
+   just compile with gcc -o pipeline pipeline.c			
+   and execute using							
+   ./pipeline  /afs/cs.pitt.edu/courses/1541/short_traces/sample.tr	0  
+***************************************************************/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include "CPU.h" 
+
+int main(int argc, char **argv)
+{
+  struct trace_item *tr_entry;
+  size_t size;
+  char *trace_file_name;
+  int trace_view_on = 0;
+  
+  unsigned char t_type = 0;
+  unsigned char t_sReg_a= 0;
+  unsigned char t_sReg_b= 0;
+  unsigned char t_dReg= 0;
+  unsigned int t_PC = 0;
+  unsigned int t_Addr = 0;
+
+  unsigned int cycle_number = 0;
+
+  if (argc == 1) {
+    fprintf(stdout, "\nUSAGE: tv <trace_file> <switch - any character>\n");
+    fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
+    exit(0);
+  }
+    
+  trace_file_name = argv[1];
+  if (argc == 3) trace_view_on = atoi(argv[2]) ;
+
+  fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
+
+  trace_fd = fopen(trace_file_name, "rb");
+
+  if (!trace_fd) {
+    fprintf(stdout, "\ntrace file %s not opened.\n\n", trace_file_name);
+    exit(0);
+  }
+
+  trace_init();
+
+  while(1) {
+    size = trace_get_item(&tr_entry);
+   
+    if (!size) {       /* no more instructions (trace_items) to simulate */
+      printf("+ Simulation terminates at cycle : %u\n", cycle_number);
+      break;
+    }
+    else{              /* parse the next instruction to simulate */
+      cycle_number++;
+      t_type = tr_entry->type;
+      t_sReg_a = tr_entry->sReg_a;
+      t_sReg_b = tr_entry->sReg_b;
+      t_dReg = tr_entry->dReg;
+      t_PC = tr_entry->PC;
+      t_Addr = tr_entry->Addr;
+    }  
+
+// SIMULATION OF A SINGLE CYCLE cpu IS TRIVIAL - EACH INSTRUCTION IS EXECUTED
+// IN ONE CYCLE
+
+    if (trace_view_on) {/* print the executed instruction if trace_view_on=1 */
+      switch(tr_entry->type) {
+        case ti_NOP:
+          printf("[cycle %d] NOP\n:",cycle_number) ;
+          break;
+        case ti_RTYPE:
+          printf("[cycle %d] RTYPE:",cycle_number) ;
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
+          break;
+        case ti_ITYPE:
+          printf("[cycle %d] ITYPE:",cycle_number) ;
+          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+          break;
+        case ti_LOAD:
+          printf("[cycle %d] LOAD:",cycle_number) ;      
+          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+          break;
+        case ti_STORE:
+          printf("[cycle %d] STORE:",cycle_number) ;      
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+          break;
+        case ti_BRANCH:
+          printf("[cycle %d] BRANCH:",cycle_number) ;
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+          break;
+        case ti_JTYPE:
+          printf("[cycle %d] JTYPE:",cycle_number) ;
+          printf(" (PC: %x)(addr: %x)\n", tr_entry->PC,tr_entry->Addr);
+          break;
+        case ti_SPECIAL:
+          printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
+          break;
+        case ti_JRTYPE:
+          printf("[cycle %d] JRTYPE:",cycle_number) ;
+          printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
+          break;
+      }
+    }
+  }
+
+  trace_uninit();
+
+  exit(0);
+}
+
+
---
+++ trace_generator.c
@@ -0,0 +1,105 @@
+/**************************************************************/
+/* CS/COE 1541				 			
+AThis program allows the user to input the instructions for a trace 
+and produce a trace file with these instructions readable by CPU.c.
+The program takes the name of the file to be generated as an argument.
+***************************************************************/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include "CPU.h" 
+
+int main(int argc, char **argv)
+{
+  struct trace_item *tr_entry=malloc(sizeof(struct trace_item));
+  size_t size;
+  char *trace_file_name;
+  int trace_view_on = 1;
+  
+  unsigned char t_type ;
+  unsigned char t_sReg_a;
+  unsigned char t_sReg_b;
+  unsigned char t_dReg;
+  unsigned int t_PC ;
+  unsigned int t_Addr ;
+
+  unsigned int cycle_number = 0;
+
+  if (argc == 1) {
+    fprintf(stdout, "\nMissing argument: the name of the file to be generated\n");
+    exit(0);
+  }
+  trace_file_name = argv[1];
+
+	int check;
+int trcount, i, repeat;
+char itype ;
+
+printf("\nHow many instructions do you want to generate: ");
+scanf("%d ", &trcount );
+printf("\n") ;
+
+for (i = 0 ; i < trcount ; i++) {
+printf("Enter the fields for instruction %d (PC itype(R|L|S|B) sReg_a sReg_b dReg addr):\n", i);
+scanf("%x %c %c %c %c %x",  &tr_entry->PC, &itype, &tr_entry->sReg_a, &tr_entry->sReg_b, &tr_entry->dReg, &tr_entry->Addr) ;
+repeat = 0 ;
+if(itype == 'R') {tr_entry->type = ti_RTYPE ;} 
+  else if (itype == 'L') {tr_entry->type = ti_LOAD;} 
+  else if (itype == 'S') {tr_entry->type = ti_STORE;} 
+  else if (itype == 'B') {tr_entry->type = ti_BRANCH ;} 
+  else {printf("unrecognized instruction type\n") ; repeat = 1;  i-- ; }
+if (repeat == 0) write_trace(*tr_entry, trace_file_name);
+} 
+trace_fd = fopen(trace_file_name, "rb");
+trace_init();
+  while(1) {
+    size = trace_get_item(&tr_entry);
+   
+    if (!size) {       /* no more instructions (trace_items) to simulate */
+      printf("+ Simulation terminates at cycle : %u\n", cycle_number);
+      break;
+    }
+    else{              /* parse the next instruction to simulate */
+      cycle_number++;
+      t_type = tr_entry->type;
+      t_sReg_a = tr_entry->sReg_a;
+      t_sReg_b = tr_entry->sReg_b;
+      t_dReg = tr_entry->dReg;
+      t_PC = tr_entry->PC;
+      t_Addr = tr_entry->Addr;
+    }  
+
+// Display the generated trace 
+
+    if (trace_view_on) {/* print the executed instruction if trace_view_on=1 */
+      switch(tr_entry->type) {
+        case ti_NOP:
+          printf("[cycle %d] NOP:",cycle_number) ;
+          break;
+        case ti_RTYPE:
+          printf("[cycle %d] RTYPE:",cycle_number) ;
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
+          break;
+        case ti_LOAD:
+          printf("[cycle %d] LOAD:",cycle_number) ;      
+          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+          break;
+        case ti_STORE:
+          printf("[cycle %d] STORE:",cycle_number) ;      
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+          break;
+        case ti_BRANCH:
+          printf("[cycle %d] BRANCH:",cycle_number) ;
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+          break;
+      }
+    }
+  }
+
+  trace_uninit();
+
+  exit(0);
+}
+
+

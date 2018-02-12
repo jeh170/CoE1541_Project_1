90e03fc731cceccd88c0a358b9eaa12ba32d88ec
jeh170 <jeh170@pitt.edu>
2018-02-11 22:12:25 -0500

    added check_branch test and fixed check_prediciton

---
+++ Predict_Test.c
@@ -0,0 +1,47 @@
+#include "branch_predict.h"
+
+int run_test(int (*t)(), int test_num);
+
+int check_branch_taken_TEST();
+int check_branch_nottaken_TEST();
+
+int main(){
+    run_test((*check_branch_taken_TEST), 1);
+    run_test((*check_branch_nottaken_TEST), 2);
+}
+
+int run_test(int (*t)(), int test_num){
+    if (t())
+        printf("%i: PASSED\n", test_num);
+    else
+        printf("%i: FAILED\n", test_num);
+}
+
+int check_branch_taken_TEST(){
+    struct trace_item stages[7];
+    struct trace_item if2, id;
+    if2.PC = 1234;
+    id.Addr = 1234;
+
+    stages[1] = if2;
+    stages[2] = id;
+
+    if (check_branch(stages))
+        return -1;
+    return 0;
+}
+
+int check_branch_nottaken_TEST(){
+    struct trace_item stages[7];
+    struct trace_item if2, id;
+    if2.PC = 1234;
+    id.Addr = 4321;
+
+    stages[1] = if2;
+    stages[2] = id;
+
+    if (!check_branch(stages))
+        return -1;
+    return 0;
+}
+
--- branch_predict.h
+++ branch_predict.h
@@ -7,7 +7,7 @@
 const struct trace_item FLUSHED = { .type = ti_FLUSHED };
 
 int branch_predict(struct trace_item stages[], int mode){
-
+    
 }
 
 int flush_stages(struct trace_item stages[]){
@@ -20,9 +20,15 @@ int flush_stages(struct trace_item stages[]){
 
 // Returns 1 if to be taken, 0 if not to be taken
 int check_branch(const struct trace_item stages[]){
-    
+    int target_addr = stages[2].Addr;
+    int next_addr = stages[1].PC;
+
+    if (target_addr == next_addr)
+        return -1;
+    else 
+        return 0;
 }
 
-int get_prediction(const struct trace_item stages[]){
-    return 0;
+int check_prediction(const struct trace_item stages[], int branch_result, int branch_pc){
+    return branch_result == 0;
 }

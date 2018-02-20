6551aad9d3252525b8c4293963d62c3797046310
orlandronen1 <roo18@pitt.edu>
2018-02-19 19:23:37 -0500

    Add files via upload

--- CPU.c
+++ CPU.c
@@ -11,7 +11,9 @@
 #include <Winsock2.h>
 #include "CPU.h" 
 
-struct trace_item empty = {
+int main(int argc, char **argv)
+{
+	const struct trace_item empty = {
 	.type = ti_EMPTY,
 	.sReg_a = 255,
 	.sReg_b = 255,
@@ -19,12 +21,10 @@ struct trace_item empty = {
 	.PC = 0,
 	.Addr = 0
 };
-struct trace_item *stages[7] = {&empty, &empty, &empty, &empty, &empty, &empty, &empty};
+struct trace_item stages[7] = {empty, empty, empty, empty, empty, empty, empty};
 
-int main(int argc, char **argv)
-{
   struct trace_item *tr_entry;
-  struct trace_item *out;
+  struct trace_item out;
   size_t size;
   char *trace_file_name;
   int trace_view_on = 0;
@@ -45,10 +45,11 @@ int main(int argc, char **argv)
     fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
     exit(0);
   }
-    
+  
   trace_file_name = argv[1];
   prediction_mode = atoi(argv[2]);
   if (argc == 4) trace_view_on = atoi(argv[3]);
+  set_up_table(64);
 
   fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
 
@@ -62,10 +63,10 @@ int main(int argc, char **argv)
   trace_init();
 
   while(1) {
-  	cycle_number++;
+  	
   	//printf("Cycle number: %d\n", cycle_number);
     /* hazard detection */ 
-    int hazard_detected = hazard_detect(stages, prediction_mode, out); 
+    int hazard_detected = hazard_detect(stages, prediction_mode, &out); 
     //printf("hazard detection done\n");   
     if (hazard_detected == hz_CTRL) 
     	delays = 3;
@@ -86,11 +87,11 @@ int main(int argc, char **argv)
       size = trace_get_item(&tr_entry);
 
       if (!size) {
-        if (stages[WB]->type == ti_EMPTY) {
+        if ((stages[WB].type == ti_EMPTY) && out.type == ti_EMPTY) {
           printf("+ Simulation terminates at cycle : %u\n", cycle_number);
           break;
         }
-        stages[IF1]->type = ti_EMPTY;
+        stages[IF1].type = ti_EMPTY;
       }
       else {
     	  t_type = tr_entry->type;
@@ -99,46 +100,48 @@ int main(int argc, char **argv)
     	  t_dReg = tr_entry->dReg;
     	  t_PC = tr_entry->PC;
     	  t_Addr = tr_entry->Addr;
-    	  stages[IF1] = tr_entry;
+    	  stages[IF1] = *tr_entry;
       }
     }
+    cycle_number++;
 
+    print_stages(stages);
 
-    if (trace_view_on && out->type != ti_EMPTY) {/* print the executed instruction if trace_view_on=1 */
-      switch(out->type) {
+    if (trace_view_on && out.type != ti_EMPTY) {/* print the executed instruction if trace_view_on=1 */
+      switch(out.type) {
         case ti_NOP:
           printf("[cycle %d] NOP\n:",cycle_number) ;
           break;
         case ti_RTYPE:
           printf("[cycle %d] RTYPE:",cycle_number) ;
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", out.PC, out.sReg_a, out.sReg_b, out.dReg);
           break;
         case ti_ITYPE:
           printf("[cycle %d] ITYPE:",cycle_number) ;
-          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", out.PC, out.sReg_a, out.dReg, out.Addr);
           break;
         case ti_LOAD:
           printf("[cycle %d] LOAD:",cycle_number) ;      
-          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", out.PC, out.sReg_a, out.dReg, out.Addr);
           break;
         case ti_STORE:
           printf("[cycle %d] STORE:",cycle_number) ;      
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", out.PC, out.sReg_a, out.sReg_b, out.Addr);
           break;
         case ti_BRANCH:
           printf("[cycle %d] BRANCH:",cycle_number) ;
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", out.PC, out.sReg_a, out.sReg_b, out.Addr);
           break;
         case ti_JTYPE:
           printf("[cycle %d] JTYPE:",cycle_number) ;
-          printf(" (PC: %x)(addr: %x)\n", tr_entry->PC,tr_entry->Addr);
+          printf(" (PC: %x)(addr: %x)\n", out.PC,out.Addr);
           break;
         case ti_SPECIAL:
           printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
           break;
         case ti_JRTYPE:
           printf("[cycle %d] JRTYPE:",cycle_number) ;
-          printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
+          printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", out.PC, out.dReg, out.Addr);
           break;
       }
     }
--- CPU.h
+++ CPU.h
@@ -128,45 +128,92 @@ int write_trace(struct trace_item item, char *fname)
 	return 1;
 }
 
-int push_stages_from(struct trace_item *stages[], int pushFrom, struct trace_item *out) {
-	out = stages[WB];
-	for (int i = WB; i > pushFrom; i--) {
-		stages[i] = stages[i-1];	// push non-stalled instrs down pipeline
+int print_stages(struct trace_item stages[])
+{
+	for (int i = 0; i < 7; i++)
+	{
+		switch(stages[i].type)
+		{
+			case ti_NOP:
+			printf("N ");
+			break;
+			case ti_LOAD:
+			printf("L ");
+			break;
+			case ti_RTYPE:
+			printf("R ");
+			break;
+			case ti_ITYPE:
+			printf("I ");
+			break;
+			case ti_STORE:
+			printf("S ");
+			break;
+			case ti_BRANCH:
+			printf("B ");
+			break;
+			case ti_JTYPE:
+			printf("J ");
+			break;
+			case ti_JRTYPE:
+			printf("JR ");
+			break;
+			case ti_SPECIAL:
+			printf("SP ");
+			break;
+			case ti_EMPTY:
+			printf("E ");
+			break;
+		}
 	}
-	stages[pushFrom]->type = ti_NOP;	// insert nop to stall pipeline
+
+		printf("\n");
 	return 0;
 }
 
-int branch_check(struct trace_item *stages[], int mode);
+struct trace_item push_stages_from(struct trace_item stages[], int pushFrom, struct trace_item out) {
+	out = stages[WB];
+	printf("%d\n", out.type);
+	for (int i = WB; i > pushFrom; i--) {
+		stages[i] = stages[i-1];	// push non-stalled instrs down pipeline
+	}
+	stages[pushFrom].type = ti_NOP;	// insert nop to stall pipeline
+	return out;
+}
 
-int hazard_detect(struct trace_item *stages[], int prediction_mode, struct trace_item* out) {
+int branch_check(struct trace_item stages[], int mode);
+
+int hazard_detect(struct trace_item stages[], int prediction_mode, struct trace_item *out) {
 	int detected = 0;
 	
 	// check for control hazard (if hazard while branch/jump in EX, flush first 3 instrs)
-	if (stages[EX]->type == ti_BRANCH) {
+	if (stages[EX].type == ti_BRANCH) {
 		printf("Control hazard\n");
 		detected = branch_check(stages, prediction_mode);
 	}
 
 	// check for structural hazard (instr trying to WB while decoding)
-	else if ((stages[WB]->type >= ti_RTYPE) && (stages[WB]->type <= ti_LOAD)) {
-		printf("struct hazard detected\n");
-		push_stages_from(stages, EX, out);
-		detected = hz_STRUCT;
+	else if ((stages[WB].type >= ti_RTYPE) && (stages[WB].type <= ti_LOAD)) {
+		if (stages[ID].type < 8) {
+			printf("struct hazard detected\n");
+			*out = push_stages_from(stages, EX, *out);
+			printf("%d\n", out->type);
+			detected = hz_STRUCT;
+		}
 	}
 
 	// check for data hazard (LW in EX/MEM1 or MEM1/MEM2 w/ data dependency in ID/EX)
-	else if (stages[EX]->type == ti_LOAD) { 	// which stage to check from depends on how we load the pipeline
-		if (stages[EX]->dReg == stages[ID]->dReg) {
+	else if (stages[EX].type == ti_LOAD) { 	// which stage to check from depends on how we load the pipeline
+		if (stages[EX].dReg == stages[ID].dReg) {
 			printf("data hazard detected\n");
-			push_stages_from(stages, EX, out);
+			*out = push_stages_from(stages, EX, *out);
 			detected = hz_DATA1;
 		}
 	}
-	else if (stages[MEM1]->type == ti_LOAD) {
-		if (stages[MEM1]->dReg == stages[ID]->dReg) {
+	else if (stages[MEM1].type == ti_LOAD) {
+		if (stages[MEM1].dReg == stages[ID].dReg) {
 			printf("data hazard detected\n");
-			push_stages_from(stages, EX, out);
+			*out = push_stages_from(stages, EX, *out);
 			detected = hz_DATA2;
 		}
 	}
@@ -194,10 +241,10 @@ struct trace_item FLUSHED = { .type = ti_FLUSHED };
 static int* prediction_table;
 static int table_size = 64;
 
-int flush_stages(struct trace_item *stages[]);
-int check_branch(struct trace_item *stages[]);
-int get_prediction(struct trace_item *stages[], int mode);
-int check_store_prediction(struct trace_item *stages[], int branch_result, int mode);
+int flush_stages(struct trace_item stages[]);
+int check_branch(struct trace_item stages[]);
+int get_prediction(struct trace_item stages[], int mode);
+int check_store_prediction(struct trace_item stages[], int branch_result, int mode);
 
 void set_up_table(int size)
 {
@@ -206,45 +253,31 @@ void set_up_table(int size)
 }
 
 // predicts whether a branch will be taken, affecting what gets loaded into the pipeline
-// 0 -> not taken, 1 -> taken
-int branch_predict(struct trace_item *stages[], int mode)
+// 0 . not taken, 1 . taken
+int branch_predict(struct trace_item stages[], int mode)
 {
-    if (stages[0]->type == ti_BRANCH)
+    if (stages[0].type == ti_BRANCH)
         return get_prediction(stages, mode);
     return 1;
 }
 
 // checks the prediction stored and flushes IF1/2 and ID
-int branch_check(struct trace_item *stages[], int mode)
+int branch_check(struct trace_item stages[], int mode)
 {
-    if (stages[3]->type == ti_BRANCH)
-    {	printf("Checking branch\n");
+    if (stages[3].type == ti_BRANCH)
+    {	
         int branch_result = check_branch(stages);
         if (!check_store_prediction(stages, branch_result, mode))
-            printf("checked prediction\n");
             return hz_CTRL;
     }
     return 0;
 }
 
-
-/*Utility Methods*/
-
-// flushes stages in the event of an incorrect prediction
-int flush_stages(struct trace_item *stages[])
-{
-    for (int i = 0; i < 4; i++){
-        stages[i] = &FLUSHED;
-    }
-
-    return 0;
-}
-
 // Returns 1 if to be taken, 0 if not to be taken
-int check_branch(struct trace_item *stages[])
+int check_branch(struct trace_item stages[])
 {
-    int target_addr = stages[3]->Addr;
-    int next_addr = stages[2]->PC;
+    int target_addr = stages[3].Addr;
+    int next_addr = stages[2].PC;
 
     if (target_addr == next_addr)
         return -1;
@@ -254,20 +287,17 @@ int check_branch(struct trace_item *stages[])
 //Hashes address for table
 int hash_for_table(int addr)
 {
-	printf("hashing\n");
-	int temp = addr >> 3;
-	printf("still hashing\n");
-	printf("%d\n", table_size);
-	return temp % table_size;
+	//int temp = addr >> 3;
+	//return temp % table_size;
     return (addr >> 3) % table_size;
 }
 
 // Gets the prediction for this branch or stores a new one
-int get_prediction(struct trace_item *stages[], int mode)
+int get_prediction(struct trace_item stages[], int mode)
 {
     int addr;
     if (mode != 0) 
-        addr = hash_for_table(stages[0]->PC);
+        addr = hash_for_table(stages[0].PC);
     
     switch (mode){
         case 0:
@@ -282,17 +312,14 @@ int get_prediction(struct trace_item *stages[], int mode)
 
 // The prediction in order to determine flushing
 // Returns -1 if prediction was correct, 0 if not
-int check_store_prediction(struct trace_item *stages[], int branch_result, int mode)
+int check_store_prediction(struct trace_item stages[], int branch_result, int mode)
 {
-	printf("checking stored prediction\n");
-    int addr = hash_for_table(stages[0]->PC);
+    int addr = hash_for_table(stages[0].PC);
 
     if (mode == 0){
-    	printf("no prediction mode check\n");
         return branch_result == 0;
     }
     if (mode == 1){
-    	printf("1 bit check\n");
         int prediction = prediction_table[addr];
         if (branch_result == prediction)
             return -1;
@@ -302,7 +329,6 @@ int check_store_prediction(struct trace_item *stages[], int branch_result, int m
         }
     }
     if (mode == 2){
-    	printf("1 bit check\n");
         int prediction = prediction_table[addr];
         int prediction_outcome = (prediction > 1);
         int predict_check = (branch_result == prediction_outcome);
@@ -314,10 +340,20 @@ int check_store_prediction(struct trace_item *stages[], int branch_result, int m
                 prediction_table[addr] = br2_NOT_TAKEN;
         }
         else{
-            if (prediction_outcome)
-                prediction_table[addr] = br2_TAKEN_FAIL;
-            else
-                prediction_table[addr] = br2_NOT_TAKEN_FAIL;
+            switch(prediction){
+            	case br2_NOT_TAKEN:
+            		prediction_table[addr] = br2_NOT_TAKEN_FAIL;
+            		break;
+            	case br2_NOT_TAKEN_FAIL:
+            		prediction_table[addr] = br1_TAKEN;
+            		break;
+            	case br2_TAKEN_FAIL:
+            		prediction_table[addr] = br2_NOT_TAKEN;
+            		break;
+            	case br2_TAKEN:
+            		prediction_table[addr] = br2_TAKEN_FAIL;
+            		break;
+            }
         }
         return predict_check;
     }
